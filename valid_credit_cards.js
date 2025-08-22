0421098be4f360fae0e406ed38b0f88ad0740b3c
hilrodcan <cangane.hilario@gmail.com>
2025-08-21 22:35:27 -0300

    Create valid_credit_cards.js

---
+++ valid_credit_cards.js
@@ -0,0 +1,47 @@
+const validCards = [
+  {
+    "card": "30459282278714",
+    "bank": "Desconhecido",
+    "country": "Desconhecido",
+    "brand": "diners club international",
+    "type": "credit",
+    "time": 0.6891627311706543,
+    "tries": 1
+  },
+  {
+    "card": "374319400823060",
+    "bank": "Desconhecido",
+    "country": "Desconhecido",
+    "brand": "Desconhecido",
+    "type": "Desconhecido",
+    "time": 0.5992250442504883,
+    "tries": 2
+  },
+  {
+    "card": "346655645863303",
+    "bank": "Desconhecido",
+    "country": "Desconhecido",
+    "brand": "Desconhecido",
+    "type": "Desconhecido",
+    "time": 0.744544506072998,
+    "tries": 3
+  },
+  {
+    "card": "36821036687785",
+    "bank": "Desconhecido",
+    "country": "Desconhecido",
+    "brand": "diners club international",
+    "type": "credit",
+    "time": 0.6275465488433838,
+    "tries": 4
+  },
+  {
+    "card": "3523246438172158",
+    "bank": "Desconhecido",
+    "country": "Desconhecido",
+    "brand": "Desconhecido",
+    "type": "Desconhecido",
+    "time": 0.6599311828613281,
+    "tries": 5
+  },
+];
